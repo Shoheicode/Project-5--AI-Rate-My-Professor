16c23e187c8f45c97e87692fcd7941923af1725c
ShoheiCode <ucodejasonirie@gmail.com>
2024-08-20 15:50:15 -0700

    updated landing page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 'use client'
 import NavBar from '@/components/navbar/navbar'
-import { Box, Button, Stack, TextField, Typography } from '@mui/material'
+import { Box, Button, Stack, TextField, Typography , Grid} from '@mui/material'
 import Head from 'next/head'
 import { useState } from 'react'
 import '@/app/CSS/LandingPage.css'
+import InfoCard from '@/components/infoCard/infoCard'
+import DevicesIcon from "@mui/icons-material/Devices";
+import TextsmsIcon from "@mui/icons-material/Textsms";
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
 
 export default function Home() {
 
@@ -15,20 +19,69 @@ export default function Home() {
       </Head>
 
       <NavBar />
-      
-      <Box
-        width="100vw"
-        height="100vh"
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Typography variant="h4" component="h1" gutterBottom id='generateText'>
-          AStar Rate my Professor
-        </Typography>
-        
-      </Box>
+      <Box 
+          width={"100%"}
+          height={"100vh"}
+          sx={{ textAlign: "center" }}
+          bgcolor={"black"}  
+          color={"white"}
+          margin={"0px"}
+          padding={10}
+        >
+          <Typography variant="h4" component="h1" gutterBottom id='generateText'>
+            AStar Rate my Professor
+          </Typography>
+          <Typography variant="h5" component="h2" gutterBottom className="apply">
+            The easiest way to start figuring out which professor is for you!
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ mt: 2, mr: 2 }}
+            href="/ChatBot"
+          >
+            Get Started
+          </Button>
+          <Button
+            variant="outlined"
+            color="inherit"
+            sx={{ mt: 2 }}
+            href="/learnmore"
+          >
+            Learn More
+          </Button>
+        </Box>
+
+        <Box 
+          // sx={{ my: 6 }}
+          padding={10}  
+          bgcolor={"darkblue"}
+          color={"white"}
+          height={"100vh"}
+        >
+          <Typography variant="h2" component="h2" gutterBottom className="apply">
+            Features
+          </Typography>
+          <Grid container spacing={4}>
+            {/* Feature items */}
+            <InfoCard
+              
+              icon={<TextsmsIcon />}
+              title="Text to Cards in Seconds"
+              subtitle="Transform your notes with just a few keystrokes"
+            />
+            <InfoCard
+              icon={<DevicesIcon />}
+              title="Easy Access"
+              subtitle="Flashcards are accessible anytime, anywhere."
+            />
+            <InfoCard
+              icon={<AutoAwesomeIcon />}
+              title="Harness Artificial Intelligence"
+              subtitle="Watch AI generate flashcards in seconds"
+            />
+          </Grid>
+        </Box>
     </Box>
   );
 }
